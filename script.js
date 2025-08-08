79bb594d68b05c8ee8e9a12d83b32482c0a7fa2d
Sophia Lacerda <sbuarquemagalhaes@gmail.com>
2025-08-08 16:26:39 -0300

    feat: start DOM manipulation

---
+++ script.js
@@ -0,0 +1,7 @@
+const body = document.getElementsByTagName('body')
+
+const firstBook = document.getElementById('first-book')
+const secondBook = document.getElementById('second-book')
+const thirdBook = document.getElementById('third-book')
+const fourthBook = document.getElementById('fourth-book')
+const fifthBook = document.getElementById('fifth-book')
